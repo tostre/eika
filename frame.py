bb6b62562f3af80ef8c82bbe4a3e2db7572bfe6c
tostre <marcel.mueller024@stud.fh-dortmund.de>
2018-12-23 15:42:35 +0100

    checkpoint

--- frame.py
+++ frame.py
@@ -1,17 +1,11 @@
 import tkinter as tk
 import datetime
-
-from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
-import pandas as pd
-from math import pi
-
-import matplotlib, sys
-matplotlib.use('TkAgg')
-from numpy import arange, sin, pi
-import numpy as np
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('TkAgg')
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg as FigureCanvas
 
 
 class Frame:
@@ -26,6 +20,7 @@ class Frame:
         self.logOut = None
         self.successMessage = None
         self.root = tk.Tk()
+        self.dgm = DiagramManager()
 
         # create menubar
         self.menubar = tk.Menu(self.root)
@@ -54,100 +49,21 @@ class Frame:
         # create diagram space
         self.diagram_frame = tk.Frame(self.root)
         self.diagram_frame.configure(background="green")
-        self.canvas1 = FigureCanvasTkAgg(self.get_figure(), master=self.diagram_frame) # a tk drawingare
+        self.fig = Figure()
+        self.diagram_canvas = FigureCanvas(self.dgm.get_figure(), master=self.diagram_frame) # a tk drawingare
 
         # position ui elements
-        self.chatOutWidget.grid(row=1, column=1, columnspan=2)
-        self.logWidget.grid(row=1, column=3)
+        self.chatOutWidget.grid(row=1, column=1, columnspan=2, sticky=tk.E+tk.W+tk.N+tk.S)
+        self.logWidget.grid(row=1, column=3, sticky=tk.E+tk.W+tk.N+tk.S)
         self.chatInWidget.grid(row=2, column=1, sticky=tk.W + tk.E)
         self.button.grid(row=2, column=2, sticky=tk.E + tk.W)
         #self.debugWidget.grid(row=1, column=4, columnspan=2, sticky=tk.E)
         self.diagram_frame.grid(row=1, column=4, columnspan=2, sticky=tk.E+tk.W+tk.N+tk.S)
         self.infoLabel.grid(row=2, column=4, sticky=tk.E)
-        self.canvas1.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.diagram_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
         # set subscriber list (implements observer pattern)
         self.subscribers = set()
-
-    def get_figure(self):
-        self.fig = Figure(figsize=(5, 4), dpi=100)
-        # an array of venly spaced numbers
-        self.x = np.arange(0, 3, .01)
-        self.y = np.sqrt(self.x)
-        self.z = np.exp(self.x)
-
-        self.fig, ((self.ax1, self.ax2), (self.ax3, self.ax4)) = plt.subplots(nrows=2, ncols=2)
-
-        self.ax1 = self.get_radar_chart("Bot emotional status", 221)
-        self.ax2 = (self.get_radar_chart("conversation dub", 222))
-        #self.ax2.plot(self.x, self.x)
-        self.ax3.plot(self.z, self.y)
-        self.ax4.plot(self.z, self.x)
-
-        self.ax2.set_title("conversation dub")
-        self.ax3.set_title("input analysis")
-        self.ax4.set_title("output")
-
-
-        # Benutze diese Methode für polar-Diagramme
-        #if method == 1:
-            # Both, add_axes and add_subplot add an axes to a figure. They both return a matplotlib.axes.Axes object.
-
-            # add_axes(x0, y0, width, height): Die Parameter geben die Position in der canvas ein
-            # Beispiel unten zeichnet ein axes-Objekt von der linken unteren bis in die rechte obere ecke der canvas
-            # Das axes-Objekt ist also genauso groß wie die canvas (parameter müssen eine liste sein)
-            # Wenn man die Achsen/Labels sehen will, darf das axes nicht bei 0,0 staten. Labels/Achses sind nichT Teil
-            # des axes-Objekts und sind daher nicht zu sehen wenn axes bei 0,0 startet
-            #### self.ax =self.fig.add_axes([0, 0, 1, 1]).plot(self.x, self.y)
-
-            # add,suplot(xxx): Man bestimmt wo das axes-objekt in einem virtuellen raster erscheint.
-            # Bsp: add_suplot(123): Das Raster hat 1 Zeilen und 2 Spalten und man fügt das axes an der 3. Postion ein
-            # Bsp: add_sbplot(111): Eine Zeile, eine Spalte. Axes nimmt also die ganze Flächer der canvas ein
-            # Der Vorteil dieser Methode: plt macht die Positionierung und lässt genug Platz für die Labels der Achsen etc
-            # Die sind nämlich nicht Teil des axes-Objektes und sind bei der Lösung oben daher nicht zu sehen
-            #self.ax1 = self.fig.add_subplot(221)
-            #self.ax2 = self.fig.add_subplot(222)
-            #self.ax3 = self.fig.add_subplot(223)
-            #self.ax4 = self.fig.add_subplot(224)
-
-        # Benutze diese Methode für normale Diagramme
-        #elif method == 2:
-            # Den ganzen oberen Teil kann man auch einfacher haben:
-            # Dabei werden die axes dem fig automatisch hinzugefügt
-            #### self.fig, ((self.ax1, self.ax2), (self.ax3, self.ax4)) = plt.subplots(nrows=2, ncols=2)
-            # nrows, ncols muss der Struktur der axes in den klammern ensprechen.
-            #self.fig, ((self.ax1, self.ax2, self.ax3), (self.ax4, self.ax4, self.ax4,)) = plt.subplots(nrows=2, ncols=3)
-
-
-        # In most cases, add_subplot would be the prefered method to create axes for plots on a canvas.
-        # Only in cases where exact positioning matters, add_axes might be useful.
-        return self.fig
-
-    def get_regular_chart(self, title):
-        pass
-
-    def get_radar_chart(self, title, position):
-        # Voerbereitung. Bei allen Listen muss der erste Wert am Ende wiederholt werden, damit der Kreis einmal
-        # ganz rum geht
-        self.labels = ["h", "s", "a", "f", "d"]
-        self.values = [0.7, 0.6, 0.3, 0.6, 0.5]
-        self.values.append(self.values[0])
-        self.length = len(self.labels)
-        self.angles = [n / float(self.length) * 2 * pi for n in range(self.length)]
-        self.angles += self.angles[:1]
-
-        # Erstelle radar chart
-        self.ax = plt.subplot(position, polar=True)
-        self.ax.set_title(title)
-        # Beschrifte die Achsen (x = der Kreis, y = die "Speichen"), ylim = limits der y-Achse
-        plt.xticks(self.angles[:-1], self.labels, color='grey', size=10)
-        plt.yticks([0.2, 0.4, 0.6, 0.8], [".2", ".4", ".6", ".8"], color="grey", size=8)
-        plt.ylim(0, 1)
-        # Plot data und fülle die Fläche dazwischen aus
-        self.ax.plot(self.angles, self.values, linewidth=.1)
-        self.ax.fill(self.angles, self.values, color='blue', alpha=0.1)
-
-        return self.ax
 
     # the following two function implement the observer pattern
     def register(self, controller):
@@ -196,21 +112,101 @@ class Frame:
         self.debugWidget.configure(state="disabled")
 
     def show(self):
-       # self.diagram = Diagrams()
         self.root.mainloop()
 
 
-class Diagrams:
+class DiagramManager:
 
     def __init__(self):
-        master = tk.Tk()
+        self.list_length = 5
+        # Bei allen Listen muss der erste Wert am Ende wiederholt werden, damit der Kreis einmal rum geht
+        self.polar_angles = [n / float(self.list_length) * 2 * np.pi for n in range(self.list_length)]
+        self.polar_angles += self.polar_angles[:1]
+        # position and names for the markers on the x- and y-axis
+        self.xticks_labels = ["h", "s", "a", "f", "d"]
+        self.yticks_positions = [0.2, 0.4, 0.6, 0.8]
+        self.yticks_labels = [".2", ".4", ".6", ".8"]
+        self.polar_data = None
 
-        w = tk.Canvas(master, width=200, height=100)
-        w.pack()
+    def get_figure(self):
+        # create data to display in regular diagrams
+        self.past_data = [[-4, -3, -2, -1, 0], [0.7, 0.8, 0.75, 0.6, 0.7]]
+        self.past_data2 = [[-4, -3, -2, -1, 0], [0.1, 0.2, 0.1, 0.4, 0.3]]
+        self.bar_data = [0.1, 0.2, 0.1, 0.4, 0.3]
+        # Vorbereitung. Bei allen radar-Listen muss der erste Wert am Ende wiederholt werden, damit der Kreis einmal rum geht
+        self.polar_data = [0.1, 0.6, 0.3, 0.6, 0.5]
+        self.polar_data.append(self.polar_data[0])
+        self.x = np.arange(0, 3, .01)
+        self.y = np.sqrt(self.x)
+        self.z = np.exp(self.x)
 
-        w.create_line(0, 0, 200, 100)
-        w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
+        # create figures and axes (kind of a subfigure)
+        self.fig, ((self.ax1, self.ax2), (self.ax3, self.ax4)) = plt.subplots(nrows=2, ncols=2)
+        self.make_radar_chart(self.ax1, "Input emotions", 221, self.polar_data)
+        self.make_radar_chart(self.ax2, "Input keyowords", 222, self.polar_data)
+        self.make_bar_chart(self.ax3, self.bar_data, "Hello title")
+        self.make_time_chart(self.ax4, self.past_data, self.past_data2, "Past emotional status")
 
-        w.create_rectangle(50, 25, 150, 75, fill="blue")
+        return self.fig
 
-        tk.mainloop()
+    def make_radar_chart(self, ax, title, position, polar_data):
+        # Erstelle radar chart
+        ax = plt.subplot(position, polar=True)
+        ax.set_title(title)
+        # Beschrifte die Achsen (x = der Kreis, y = die "Speichen"), ylim = limits der y-Achse
+        plt.xticks(self.polar_angles[:-1], self.xticks_labels, color='grey', size=10)
+        plt.yticks(self.yticks_positions, self.yticks_labels, color="grey", size=8)
+        ax.set_ylim(0, 1)
+        # Plot data und fülle die Fläche dazwischen aus
+        ax.plot(self.polar_angles, polar_data, alpha=1)
+        ax.fill(self.polar_angles, polar_data, color='blue', alpha=0.1)
+
+    def make_bar_chart(self, ax, bar_data, title):
+        ax.set_title(title)
+        ax.set_ylim(0, 1)
+        ax.bar(self.xticks_labels, bar_data, width=.4, color="blue")
+
+    def make_time_chart(self, ax, past_data, past_data2, title):
+        ax.set_title(title)
+        ax.yaxis.tick_right()
+        ax.set_ylim(0, 1)
+        ax.set_xlim(-4, 0)
+        ax.plot(past_data[0], past_data[1])
+        ax.plot(past_data2[0], past_data2[1])
+
+    def update_radar_chart(self, values):
+        pass
+
+    def old(self):
+        # tutorial commands for using axes/suplots etc
+        # Benutze diese Methode für polar-Diagramme
+        # if method == 1:
+        # Both, add_axes and add_subplot add an axes to a figure. They both return a matplotlib.axes.Axes object.
+
+        # add_axes(x0, y0, width, height): Die Parameter geben die Position in der canvas ein
+        # Beispiel unten zeichnet ein axes-Objekt von der linken unteren bis in die rechte obere ecke der canvas
+        # Das axes-Objekt ist also genauso groß wie die canvas (parameter müssen eine liste sein)
+        # Wenn man die Achsen/Labels sehen will, darf das axes nicht bei 0,0 staten. Labels/Achses sind nichT Teil
+        # des axes-Objekts und sind daher nicht zu sehen wenn axes bei 0,0 startet
+        #### self.ax =self.fig.add_axes([0, 0, 1, 1]).plot(self.x, self.y)
+
+        # add,suplot(xxx): Man bestimmt wo das axes-objekt in einem virtuellen raster erscheint.
+        # Bsp: add_suplot(123): Das Raster hat 1 Zeilen und 2 Spalten und man fügt das axes an der 3. Postion ein
+        # Bsp: add_sbplot(111): Eine Zeile, eine Spalte. Axes nimmt also die ganze Flächer der canvas ein
+        # Der Vorteil dieser Methode: plt macht die Positionierung und lässt genug Platz für die Labels der Achsen etc
+        # Die sind nämlich nicht Teil des axes-Objektes und sind bei der Lösung oben daher nicht zu sehen
+        # self.ax1 = self.fig.add_subplot(221)
+        # self.ax2 = self.fig.add_subplot(222)
+        # self.ax3 = self.fig.add_subplot(223)
+        # self.ax4 = self.fig.add_subplot(224)
+
+        # elif method == 2:
+        # Den ganzen oberen Teil kann man auch einfacher haben:
+        # Dabei werden die axes dem fig automatisch hinzugefügt
+        #### self.fig, ((self.ax1, self.ax2), (self.ax3, self.ax4)) = plt.subplots(nrows=2, ncols=2)
+        # nrows, ncols muss der Struktur der axes in den klammern ensprechen.
+        # self.fig, ((self.ax1, self.ax2, self.ax3), (self.ax4, self.ax4, self.ax4,)) = plt.subplots(nrows=2, ncols=3)
+
+        # In most cases, add_subplot would be the prefered method to create axes for plots on a canvas.
+        # Only in cases where exact positioning matters, add_axes might be useful.
+        pass
