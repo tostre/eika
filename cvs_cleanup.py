dc419b4e2ce0bc6306a4f0a27bcbfd23ba6f1e29
tostre <marcel.mueller3101@gmail.com>
2019-07-19 01:10:55 +0200

    Switched from csv to pandas. Filtering for affect works, up next: Manual cleanup (renaming some affects in some datasets), filtering for links and mentions

--- cvs_cleanup.py
+++ cvs_cleanup.py
@@ -1,28 +1,48 @@
 import csv
+import pandas
 
 class Cleaner:
     def __init__(self):
         self.datasets = [["crowdflower", ","], ["emoint", "\t"],
                          ["emotion_classification", ","],
                          ["isear", "\t"], ["meld", ","], ["tec", "\t"]]
-        #self.datasets = ["emoint"]
+        #self.datasets = [["crowdflower", ","]]
         self.emotions = ["happiness", "sadness", "anger", "fear", "disgust"]
         self.test_emotions = ["neutral", "happiness"]
         self.test_line = ["greeting you all a happy mother's day!", 'happiness']
 
 
+
+
         #self.lists_have_common_member(self.emotions, self.test_line)
 
-        self.clean_delimiter()
-
+        #self.clean_delimiter()
+        self.pandas_teset()
         pass
+
+    def pandas_teset(self):
+        for dataset in self.datasets:
+            print("cleaning up dataset", dataset[0])
+            # Read the dataset
+            self.dataset = pandas.read_csv("corpora/" + dataset[0] + ".csv", delimiter=dataset[1])
+            # Choose only the rows annotated with 1 of the 5 emotions
+            self.emotion_rows = self.dataset.loc[(self.dataset["affect"] == "happiness") |
+                                      (self.dataset["affect"] == "sadness") |
+                                      (self.dataset["affect"] == "anger") |
+                                      (self.dataset["affect"] == "fear") |
+                                      (self.dataset["affect"] == "disgust")]
+            # Save these datasets as a cleaned up corpus
+            self.emotion_rows.to_csv("corpora/" + dataset[0] + "_clean.csv", sep=",")
+            print("... finished")
+
+
 
     # replaces any other delimiter with a comma
     def clean_delimiter(self):
         for dataset in self.datasets:
-            with open("korpora/" + dataset[0] + ".csv", "r") as file:
+            with open("corpora/" + dataset[0] + ".csv", "r") as file:
                 reader = csv.reader(file, delimiter=dataset[1])
-                with open("korpora/" + dataset[0] + "_clean.csv", "w") as new_file:
+                with open("corpora/" + dataset[0] + "_clean.csv", "w") as new_file:
                     writer = csv.writer(new_file, delimiter=",")
 
                     for line in reader:
