67ef84e6398db59fa8574b49114b0468bcfa6ec4
tostre <marcel.mueller3101@gmail.com>
2019-07-21 15:38:22 +0200

    Special characters are replaces by proper charcters, hashtags will be removed

--- modules/csv_mod.py
+++ modules/csv_mod.py
@@ -8,10 +8,11 @@ class Cleaner:
         self.drop_rows = None
         self.clean_rows = None
         self.emotions = ["happiness", "sadness", "anger", "fear", "disgust"]
-        self.datasets = [["crowdflower", ","], ["emoint", "\t"],
+        self.datasets = [["test", ","], ["crowdflower", ","], ["emoint", "\t"],
                          ["emotion_classification", ","],
                          ["isear", "\t"], ["meld", ","], ["tec", "\t"]]
         #self.datasets = [["crowdflower", ","]]
+        self.datasets = [["test", ","]]
 
         self.cleanup_datasets()
 
@@ -36,10 +37,20 @@ class Cleaner:
             print("... removing links and mentions")
             self.drop_rows = []
             for index, row in self.clean_rows.iterrows():
-                if row["text"].__contains__("http") or row["text"].__contains__("@"):
+                if row["text"].__contains__("http") \
+                        or row["text"].__contains__("@")\
+                        or row["text"].__contains__("#"):
                     self.drop_rows.append(False)
                 else:
                     self.drop_rows.append(True)
+
+                    # Replace some strings
+                    self.clean_rows.iloc[index, 1] = self.clean_rows.iloc[index, 1]\
+                        .replace("&quot", "\"")\
+                        .replace("&amp", "&")\
+                        .replace("&lt", "<")\
+                        .replace("&gt", ">")
+
             self.drop_rows = pandas.Series(self.drop_rows)
             # Choose only the rows that don't contain links or mentions
             self.clean_rows = self.clean_rows[self.drop_rows]
@@ -48,6 +59,17 @@ class Cleaner:
             self.clean_rows.reset_index(inplace=True, drop=True)
             self.clean_rows.to_csv("corpora/" + dataset[0] + "_clean.csv", sep=",")
             print("... done\n")
+
+            # @todo
+            # vlt kann ich diese datensätze nicht nehmen:
+            # isear: beschreibungen von vorfällen
+            # crowdflower: sind hashtags drin
+            # emoint (hashtags)
+            # meld (sind keine chatnachrichten, sondern sauber abgetippte tv-dialoge)
+            # tec (hashtags, außerdem spanische zeilen)
+
+            # features: Shape, anzahl der satzzeichen
+
 
 
 cleaner = Cleaner()
